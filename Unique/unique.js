0c1b04390a87e6d0abdf8f6872ba2e4dfa7233d9
Maiye123 <olorunmaiyeoluwakemi@gmail.com>
2023-12-22 15:05:11 -0800

    Unique

--- Unique/unique.js
+++ Unique/unique.js
@@ -1,14 +1,9 @@
-function sumEvenNumbers(numbers) {
-    let sum = 0;
-    for (const number of numbers) {
-      if (typeof number === 'number' && number % 2 === 0) {
-        sum += number;
-      }
-    }
-    return sum;
-  }
-  
-  // Sample:
-  const numbersArray = [2, 5, 8, 10, 15];
-  const result = sumEvenNumbers(numbersArray);
-  console.log(result);
+// A unique function that adds two numbers
+
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Sample:
+const result = addNumbers(6, 9);
+console.log(result);
